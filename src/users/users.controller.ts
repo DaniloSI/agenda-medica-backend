527cd336fe6b02a5f60db0cfec69b78bbcde1385
Danilo de Oliveira <danilo.oliveira@grupoboticario.com.br>
2023-05-29 19:50:28 -0300

    Initial

---
+++ src/users/users.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { SignUpDto } from './dto/sign-up.dto';
+import { User } from './models/user.model';
+import { SignInDto } from './dto/sign-in.dto';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
+  public async signUp(@Body() signUpDto: SignUpDto): Promise<User> {
+    return this.usersService.signUp(signUpDto);
+  }
+
+  @Post('signin')
+  @HttpCode(HttpStatus.OK)
+  public async signIn(@Body() signInDto: SignInDto): Promise<{
+    name: string;
+    email: string;
+    jwtToken: string;
+  }> {
+    return this.usersService.signIn(signInDto);
+  }
+
+  @Get()
+  @UseGuards(AuthGuard('jwt'))
+  @HttpCode(HttpStatus.OK)
+  public async findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+}
